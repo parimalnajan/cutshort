65e6798ee3c3faaf10ed82263e5c61bfae3202e6
Parimal Najan <63017672+parimalnajan@users.noreply.github.com>
2022-10-20 20:45:38 +0530

    slight change to progress bar styling

--- src/components/ProgressIndicator.jsx
+++ src/components/ProgressIndicator.jsx
@@ -5,16 +5,16 @@ import { StepContext } from '../pages/onboarding';
 const ProgressCircle = ({ currentStep, iterator ,lastStep }) => {
   return (
     <div className="flex flex-row items-center text-gray-500 ">
-      <div className={`${iterator<=currentStep?"bg-brand text-gray-300":""} border-gray-200 border-2 rounded-full w-10 h-10 flex flex-row items-center justify-center`}>
+      <div className={`${iterator<=currentStep?"bg-brand text-gray-300 ":""} border-gray-100 border-2 rounded-full w-10 h-10 flex flex-row items-center justify-center`}>
         <div className="text-xs font-semibold">{iterator}</div>
       </div>
 
     <div> 
-        <div style={{ height: "1.5px" }} className={`bg-gray-200 ${iterator==lastStep?"w-0":"w-14"}`}></div> 
+        <div style={{ height: "1.5px" }} className={`bg-gray-200 ${iterator==lastStep?"w-0":"w-16"}`}></div> 
         <div
           style={{ height: "0.5px", bottom: "0.9px" }}  // "track" and "thumb"
           className={`bg-brand relative left-0     
-          ${iterator==lastStep?"w-0":iterator < currentStep ? "w-14":iterator==currentStep?"w-7" :iterator>currentStep?"w-0":""}`}   // thumb render as empty/half/full as per state
+          ${iterator==lastStep?"w-0":iterator < currentStep ? "w-16":iterator==currentStep?"w-8" :iterator>currentStep?"w-0":""}`}   // thumb render as empty/half/full as per state
         ></div>
       </div>
     </div>
